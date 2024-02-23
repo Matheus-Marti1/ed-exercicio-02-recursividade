cfb523c60025bc47c3f10bd2e52a6505f64220a9
Matheus Marti <0555147+Matheus-Marti1@users.noreply.github.com>
2024-02-23 10:48:53 -0300

    Melhorar formatação do código

--- src/controller/MenorValorController.java
+++ src/controller/MenorValorController.java
@@ -4,13 +4,15 @@ public class MenorValorController {
 	public MenorValorController() {
 		super();
 	}
-	
-	public int menorValor (int[] vet, int tamanho, int menorValor) {
-		// A condição de parada será quando o vetor não tiver mais posições, e o retorno será o menor valor encontrado
+
+	public int menorValor(int[] vet, int tamanho, int menorValor) {
+		// A condição de parada será quando o vetor não tiver mais posições, e o retorno
+		// será o menor valor encontrado
 		if (tamanho == 0) {
 			return menorValor;
 		} else {
-			// Enquanto a função não tiver chegado na condição de parada, o código irá fazer uma verificação do valor contido no vetor na posição atual,
+			// Enquanto a função não tiver chegado na condição de parada, o código irá fazer
+			// uma verificação do valor contido no vetor na posição atual,
 			// e caso o valor seja menor que o valor já encontrado, irá substituí-lo.
 			int ultimaPosicao = tamanho - 1;
 			if (vet[ultimaPosicao] <= menorValor) {
@@ -18,7 +20,8 @@ public class MenorValorController {
 			}
 			tamanho = tamanho - 1;
 			return menorValor(vet, tamanho, menorValor);
-			// O retorno será a chamada da própria função, com o vetor, a posição reduzida em 1, e o menor valor encontrado até agora
+			// O retorno será a chamada da própria função, com o vetor, a posição reduzida
+			// em 1, e o menor valor encontrado até agora
 		}
 	}
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -6,9 +6,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		MenorValorController menorValorController = new MenorValorController();
-		int vet[] = {4, 81, 5, 25, 10, 2, 31};
+		int vet[] = { 4, 81, 5, 25, 10, 2, 31 };
 		int tamanho = vet.length;
-		int ultimoValor = vet[tamanho-1];
+		int ultimoValor = vet[tamanho - 1];
 		int menorValor = menorValorController.menorValor(vet, tamanho, ultimoValor);
 		System.out.println("Menor valor do vetor: " + menorValor);
 	}
